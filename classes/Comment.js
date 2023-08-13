6a119bb6cdddf2ce0e472d60b5d0630708b4af05
Till1989 <131892010+Till1989@users.noreply.github.com>
2023-08-13 15:11:26 +0300

    1

--- classes/Comment.js
+++
@@ -1,7 +0,0 @@
-export class Comment {
-    constructor(user, text) {
-        this.user = user;
-        this.text = text;
-    }
-}
-//# sourceMappingURL=Comment.js.map
--- classes/User.js
+++
@@ -1,7 +0,0 @@
-export class User {
-    constructor(name, type) {
-        this.name = name;
-        this.isAnon = type;
-    }
-}
-//# sourceMappingURL=User.js.map
