3811c8dcaaaa3b08450da4cfc93daf941f546256
Till <stmila32@gmail.com>
2023-09-21 20:40:51 +0300

    1

--- classes/Customer.ts
+++ classes/Customer.ts
@@ -2,26 +2,28 @@ import { POSTerminal } from "./POSTerminal.js"; ////
 
 export abstract class Customer {
     name: string;
-    identifier: number;
     taxID: number;
-    constructor(name: string, identifier: number, taxID: number) {
+    constructor(name: string, taxID: number) {
         this.name = name;
-        this.identifier = identifier;
         this.taxID = taxID;
     }
 }
 
 export class Person extends Customer {
     pass: string;
+    identifier: number;
     constructor(name: string, identifier: number, pass: string, taxID: number) {
-        super(name, identifier, taxID);
+        super(name, taxID);
+        this.identifier = identifier;
         this.pass = pass;
     }
 }
 export class Business extends Customer {
     POSTerminals: POSTerminal[] = [];
+    identifier: number;
     constructor(name: string, identifier: number, taxID: number) {
-        super(name, identifier, taxID);
+        super(name, taxID);
+        this.identifier = identifier;
     }
 
     addPOSTerminal(POSTerminal: POSTerminal): void {
